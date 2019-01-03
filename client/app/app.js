2e200b242f3e28995e4494f9052da3c48c9bb3c8
Leo Casagrande <leo@greenberry.io>
2019-01-03 17:02:44 +0100

    map

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,7 @@
 /**
  * Déclaration de l'application demoApp
  */
-angular.module('app', ['ngRoute','ngResource','chart.js']);
+angular.module('app', ['ngRoute','ngResource','chart.js','leaflet-directive']);
 
 /**
  * Déclaration du module todoList
--- client/app/controller/device_detail.controller.js
+++ client/app/controller/device_detail.controller.js
@@ -1,31 +1,101 @@
 angular.module("app").controller("devices_detailsCtrl", ["$scope", "$routeParams", "DevicesService", "SensorsService", function($scope, $routeParams, DevicesService, SensorsService) {
+    var mainMarker = {
+        lat: 49.114821,
+        lng: 6.181632,
+        focus: true,
+        message: "IFA !!",
+        draggable: false
+    };
+
+    angular.extend($scope, {
+        defaults: {
+            scrollWheelZoom: false
+        },
+        london: {
+            lat: 49.114821,
+            lng: 6.181632,
+            zoom: 18
+        },
+        markers: {
+            mainMarker: angular.copy(mainMarker)
+        },
+    });
     $scope.monDevice = DevicesService.get({
         "devicesId": $routeParams.deviceId
     });
     $scope.mesSensors = SensorsService.query();
+    $scope.labels = [];
+    $scope.data = [];
+    $scope.options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }],
+            xAxes: [{
+                // type: 'time',
+                // time: {
+                //     unit: 'minute'
+                // }
+            }]
+        },
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Custom Chart Title'
+        }
+
+    };
 
     socket.on("newSensor", function(socket) {
         console.log(socket);
         $scope.mesSensors.unshift(socket);
         console.log($scope.mesSensors);
-        $scope.$apply();
 
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.series = ['Series A', 'Series B'];
-        $scope.data = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
-        ];
+
+        if (socket.name === "analogValue") {
+
+            $scope.labels.push(moment(socket.published_at).format("YY-MM-DD HH:mm' s''"));
+            $scope.data.push(socket.data);
+
+        }
+        if (socket.name === "ubidotsLatLong") {
+            var datas = JSON.parse(socket.data)
+
+            // var ts = moment(datas.timestamp).fromNow();
+
+            var m = moment.utc(datas.timestamp); // input is local time
+            console.log(m);
+            m.tz('Europe/Paris'); // convert to UTC+1
+            var ts = m.format("LLL");
+            console.log(ts);
+            mainMarker = {
+                lat: parseFloat(datas["google-lat"]),
+                lng: parseFloat(datas["google-lng"]),
+                focus: true,
+                message: "lat : " + datas["google-lat"] + " lng : " + datas["google-lng"] + " accuracy :" + datas['google-acc'] + " at : " + ts,
+                draggable: false
+            };
+
+            angular.extend($scope, {
+
+                markers: {
+                    mainMarker: angular.copy(mainMarker)
+                },
+                london: {
+                    lat: parseFloat(datas["google-lat"]),
+                    lng: parseFloat(datas["google-lng"]),
+                    zoom: 18
+                },
+            });
+        }
+        $scope.$apply();
         $scope.onClick = function(points, evt) {
             console.log(points, evt);
         };
 
         // Simulate async data update
-        $timeout(function() {
-            $scope.data = [
-                [28, 48, 40, 19, 86, 27, 90],
-                [65, 59, 80, 81, 56, 55, 40]
-            ];
-        }, 3000);
+
     });
 }]);
